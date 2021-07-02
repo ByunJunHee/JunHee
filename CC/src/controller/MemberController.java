7928d22a3aa4154c0a61efba18e52e9893f91428
747 <747@DESKTOP-R2M5S6Q>
2021-07-02 16:05:30 +0900

    yy

--- CC/src/controller/MemberController.java
+++ CC/src/controller/MemberController.java
@@ -13,101 +13,118 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import service.MemberDao;
-import service.MemberVo;
+import member.MemberDAO;
+import member.MemberVO;
+
 
 
 @WebServlet("/MemberController")
-public class MemberController extends HttpServlet {
+public class MemberController1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=utf-8");
 		
-		String uri = request.getRequestURI();		
+		
+		String uri = request.getRequestURI();	
+		
+		System.out.println("uri1 =>:"+uri);
+		
+		
 		int pnamelength = request.getContextPath().length();
 		String str = uri.substring(pnamelength);
-		System.out.println("url주소:"+str);
 		
-		if(str.equals("/memberWriteAction.do")) {
+		System.out.println("url�ּ�:"+str);
+		
+	if(str.equals("/memberJoinAction.do")){
 			
-			String memberId = request.getParameter("memberId");
-			String memberPwd = request.getParameter("memberPwd");
-			String memberName = request.getParameter("memberName");
-			String memberEmail = request.getParameter("memberEmail");
-			String memberJumin = request.getParameter("memberJumin");
-			String memberPhone = request.getParameter("memberPhone");
-			String memberAddr = request.getParameter("memberAddr");
-			String memberGender = request.getParameter("memberGender");
+			String m_id = request.getParameter("m_id");
+			String m_pass = request.getParameter("m_pass");
+			String m_name = request.getParameter("m_name");
+			String m_gender = request.getParameter("m_gender");
+			String m_phone1 = request.getParameter("m_phone1");
+			String m_phone2 = request.getParameter("m_phone2");
+			String m_email = request.getParameter("m_email");
+			String m_addr1 = request.getParameter("m_addr1");
+			String m_addr2 = request.getParameter("m_addr2");
+			String m_addr3 = request.getParameter("m_addr3");
+			
+			System.out.println("m_id = "+m_id);
+			System.out.println("m_pass = "+m_pass);
+			System.out.println("m_name = "+m_name);
+			System.out.println("m_gender = "+m_gender);
+			System.out.println("m_phone1 = "+m_phone1);
+			System.out.println("m_phone2 = "+m_phone2);
+			System.out.println("m_email = "+m_email);
+			System.out.println("m_addr1 = "+m_addr1);
+			System.out.println("m_addr2 = "+m_addr2);
+			System.out.println("m_addr3 = "+m_addr3);
+			
 			
 			String ip = InetAddress.getLocalHost().getHostAddress();
-
-			//�޼ҵ� ȣ��
-			MemberDao md = new MemberDao();
-			md.memberInsert(memberName, memberId, memberPwd, memberJumin, memberAddr, memberEmail, memberPhone, memberGender, ip);
-					
-			response.sendRedirect(request.getContextPath()+"/memberWrite.do");
 			
-		}else if (str.equals("/memberWrite.do")) {
+			MemberDAO md = new MemberDAO();
+			md.memberInsert(m_id, m_pass, m_name, m_gender, m_phone1, m_phone2, m_email, m_addr1, m_addr2, m_addr3);
 			
-			RequestDispatcher rd = request.getRequestDispatcher("/memberWrite.jsp");
-			rd.forward(request, response);				
+			response.sendRedirect(request.getContextPath()+"/memberJoinAction.jsp");
+		
+		}else if(str.equals("/memberJoin.do")) {
 			
-		}else if (str.equals("/memberList.do")) {
-			
-			MemberDao md = new MemberDao();
-			ArrayList<MemberVo> alist = md.memberSelectAll();
-			
-			request.setAttribute("alist", alist);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/memberList.jsp");
-			rd.forward(request, response);				
-
-			
+			RequestDispatcher rd = request.getRequestDispatcher("/memberJoin.jsp");
+			rd.forward(request, response);	
 		}else if(str.equals("/memberLogin.do")) {
 			
 			RequestDispatcher rd = request.getRequestDispatcher("/memberLogin.jsp");
 			rd.forward(request, response);
+		
+		}else if(str.equals("/main.do")) {
 			
-		}else if(str.equals("/memberLoginAction.do")) {
-
-			String memberId = request.getParameter("memberId");
-			String memberPwd = request.getParameter("memberPwd");
-			System.out.println("memberId"+memberId);
+			RequestDispatcher rd = request.getRequestDispatcher("/main.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/introduce.do")) {
 			
-			MemberDao md = new MemberDao();
-			int midx = md.memberLoginCheck(memberId, memberPwd);
+			RequestDispatcher rd = request.getRequestDispatcher("/introduce.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/findId.do")) {
 			
-			PrintWriter out = response.getWriter();
-			if(midx > 0) {	//로그인한 id와 같은 사람이 존재한다면
-				//세션변수에 값을 담는다.
-				HttpSession session = request.getSession();
-				session.setAttribute("S_memberId", memberId);
-				session.setAttribute("midx", midx);
-				
-				out.println("<script>alert('로그인하셨습니다.');document.location.href='./memberList.do';</script>");
-			}else {
-				out.println("<script>alert('잘못된 로그인입니다.');document.location.href='./memberLogin.do';</script>");
-			}
+			RequestDispatcher rd = request.getRequestDispatcher("/findId.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/findPw.do")) {
 			
+			RequestDispatcher rd = request.getRequestDispatcher("/findPw.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/memberLoginOk.do")) {
 			
-			
+			RequestDispatcher rd = request.getRequestDispatcher("/memberLoginOk.jsp");
+			rd.forward(request, response);
+		
 		}else if(str.equals("/memberLogout.do")) {
-			HttpSession session = request.getSession();
-			session.invalidate();
-			PrintWriter out = response.getWriter();
-			out.println("<script>alert('로그아웃 하셧습니다');document.location.href='"+request.getContextPath()+"/';</script>");
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/memberLogout.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/findIdOk.do")) {
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/findIdOk.jsp");
+			rd.forward(request, response);
+		
+		}else if(str.equals("/findPwOk.do")) {
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/findPwOk.jsp");
+			rd.forward(request, response);
 		}
-		
-		
 	}
+	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		
 		doGet(request, response);
 	}
-
 }
